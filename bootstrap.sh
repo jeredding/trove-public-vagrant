40cfb91c3624c295ef37607be40b6735c47ec6ce
Erik Redding <erik.redding@rackspace.com>
2013-09-01 22:35:11 -0500

    added more stuff for testing functionality

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,14 +15,14 @@ echo "---> setup and install some things:"
 echo "ignore source repos for now..."
 sed -i  -e 's/^\(deb-src.*\)/#\1/g' /etc/apt/sources.list
 apt-get update 
-apt-get install -y  build-essential python-setuptools python-software-properties python-dev
+apt-get install -y  build-essential python-setuptools python-software-properties python-dev libmysqlclient-dev libxml2-dev libxslt1-dev git
 easy_install pip 
 pip install tox
 echo "get ppa for python2.6"
 add-apt-repository --yes ppa:j5-dev/python2.6
 apt-get update 
-apt-get install -y python2.6 python2.6-dev libmysqlclient-dev libxml2-dev libxslt1-dev
+apt-get install -y python2.6 python2.6-dev 
 echo "lets get started..."
 echo 'PATH=/sbin/:/usr/sbin/:/usr/local/bin:/usr/local/sbin:$PATH;' >> /home/vagrant/.bash_profile
 pushd /src/trove/
-su - vagrant -c "tox" 
+su - vagrant -c "cp -r /src/trove ~/; pushd ~/trove/; tox" 
